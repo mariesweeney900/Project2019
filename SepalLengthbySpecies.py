db5c11d12d088cc522076adec85ede3ec01d3809
mariesweeney900 <47941381+mariesweeney900@users.noreply.github.com>
2019-04-23 22:17:54 +0100

    Update SepalLengthbySpecies.py

--- SepalLengthbySpecies.py
+++ SepalLengthbySpecies.py
@@ -2,9 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 dataframe = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv')
+#Adapted from https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv
+
 x = dataframe.sepal_length
 y = dataframe.species
+
+#Adapted from https://ourcodingclub.github.io/2018/04/18/pandas-python-intro.html
+
+plt.xlabel("Sepal Length (cm)", fontsize=20)
+plt.ylabel("Species", fontsize=20)
 
 plt.scatter(x, y)
 
 plt.show()
+
+#Adapted from https://matplotlib.org/users/pyplot_tutorial.html
