bf9d4f36dbdb5c194959e194ca98a8c0c2042c2a
mariesweeney900 <47941381+mariesweeney900@users.noreply.github.com>
2019-04-23 20:12:39 +0100

    Add files via upload

---
+++ PetalWidthbySpecies.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataframe = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv')
+x = dataframe.petal_width
+y = dataframe.species
+
+plt.scatter(x, y)
+
+plt.show()
