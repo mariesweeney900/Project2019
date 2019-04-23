75da14e3eaa214cbcadf4e68a4d4519e6fb8f0ac
mariesweeney900 <47941381+mariesweeney900@users.noreply.github.com>
2019-04-23 15:13:57 +0100

    Add files via upload

---
+++ SpeciesbyPetalLength.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataframe = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv')
+x = dataframe.species
+y = dataframe.petal_length
+
+plt.scatter(x, y)
+
+plt.show()
