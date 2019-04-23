5445788778ef39414515a87b070e7227828f8726
mariesweeney900 <47941381+mariesweeney900@users.noreply.github.com>
2019-04-23 22:10:27 +0100

    Update PetalLengthvSpecies.py

--- PetalLengthvSpecies.py
+++ PetalLengthvSpecies.py
@@ -2,12 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 dataframe = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv')
+#Adapted from https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv
+
 x = dataframe.petal_length
 y = dataframe.species
+#Adapted from https://ourcodingclub.github.io/2018/04/18/pandas-python-intro.html
 
-plt.xlabel("Petal length (cm)", fontsize=16)
+plt.xlabel("Petal Length (cm)", fontsize=16)
 plt.ylabel("Species", fontsize=16)
 
 plt.scatter(x, y)
 
 plt.show()
+#Adapted from https://matplotlib.org/users/pyplot_tutorial.htm
