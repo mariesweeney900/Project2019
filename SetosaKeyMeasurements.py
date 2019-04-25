197ea02369c5c39a91422beb1e0f43c65f3701b4
mariesweeney900 <47941381+mariesweeney900@users.noreply.github.com>
2019-04-25 15:31:00 +0100

    Update SetosaKeyMeasurements.py

--- SetosaKeyMeasurements.py
+++ SetosaKeyMeasurements.py
@@ -7,15 +7,13 @@ import matplotlib.pyplot as plt
 
 
 data = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv', index_col = 'species')
+#Adapted from https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv
 # read the data from the downloaded CSV file.
 
 data.loc['setosa'].plot()
-#data. is an operation that locates rows that setosa is in 
-
 plt.ylabel("Measurements (cm)", fontsize=16)
-#labels the y axis of the plot as Measurements in cm and applies fontsize of 16
-
-
-
 plot.show()
-#thia function in atplotlib shows the plot
+#data.loc is an operation that locates rows that setosa is in
+#labels the y axis of the plot as Measurements in cm and applies fontsize of 16
+#thia function in matplotlib shows the plot
+#Adapted from https://swcarpentry.github.io/python-novice-gapminder/09-plotting/?fbclid=IwAR1tJvpM5b2qMJVdFqk8AUy-tVN8JURNRrHt8N9scsRbjmb6rql_uwY-LUw
