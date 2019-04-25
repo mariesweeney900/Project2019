65f89dfd3ca348f28676793f7520a528f877c1dc
mariesweeney900 <47941381+mariesweeney900@users.noreply.github.com>
2019-04-25 15:15:39 +0100

    Add files via upload

---
+++ VirginicaKeyMeasurements.py
@@ -0,0 +1,19 @@
+import pandas as pd
+#imports pandas as pd
+
+import matplotlib.pyplot as plt
+#imports matplotlib as plt
+
+
+
+data = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv', index_col = 'species')
+# read the data from the downloaded CSV file.
+
+data.loc['virginica'].plot()
+#data. is an operation that locates rows that virginica is found
+
+plt.ylabel("Measurements (cm)", fontsize=16)
+#labels the y axis of the plot as Measurements in cm and applies fontsize of 16
+
+plot.show()
+#thia function in atplotlib shows the plot
