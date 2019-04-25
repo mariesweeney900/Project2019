847721102c1a66c4498cf35e651e8d7f00035110
mariesweeney900 <47941381+mariesweeney900@users.noreply.github.com>
2019-04-25 15:28:34 +0100

    Update VirginicaKeyMeasurements.py

--- VirginicaKeyMeasurements.py
+++ VirginicaKeyMeasurements.py
@@ -7,13 +7,13 @@ import matplotlib.pyplot as plt
 
 
 data = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv', index_col = 'species')
+#Adapted from https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv
 # read the data from the downloaded CSV file.
 
 data.loc['virginica'].plot()
-#data. is an operation that locates rows that virginica is found
-
 plt.ylabel("Measurements (cm)", fontsize=16)
-#labels the y axis of the plot as Measurements in cm and applies fontsize of 16
-
 plot.show()
-#thia function in atplotlib shows the plot
+#This is an operation  in matplotlib that locates rows that virginica is found
+#labels the y axis of the plot as Measurements in cm and applies fontsize of 16
+#this functionshows the plot
+#Adapted from 'https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv'
