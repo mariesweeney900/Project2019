89d05d91651c51e144a3f72e81e34ee36afba8f7
mariesweeney900 <47941381+mariesweeney900@users.noreply.github.com>
2019-04-28 23:32:57 +0100

    Update iris1.py

--- iris1.py
+++ iris1.py
@@ -5,14 +5,20 @@ import pandas
 import sklearn
 #import sklearn
 data = numpy.genfromtxt('data\iris.csv',delimiter=',')
+#Adapted from https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv
 #attempted to read csv file from stored file but ran in to too many issues
 data = pd.read_csv('https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv')
 #assigned data to read csv from online file downloaded to computer and worked much better
+#Adapted from https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv
 AvData = numpy.mean(data[:,1])
+#Adapted from https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.describe.html
 #gets the mean of the data from column one
 data.describe()
+#Adapted from https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.describe.html
 #gets the count, mean, standard deviation, minimum, max ad certain percentages of the data at 50, 25 and 75% of all columns of data as column 4 is string data it is not a number (nan)
 data.describe().transpose()
+#Adapted from https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.describe.html
+
 #transposes the data as not all data is numerical isolating the column that can not be summarised
 sepalmax=numpy.max(data[0,1])
 sepalmax=numpy.max(data[1,1])
@@ -22,6 +28,7 @@ sepalmax=numpy.max(data[:,2])
 sepalmax=numpy.max(data[:,0])
 #attemted to get the max of data
 df= pd.DataFrame(data)
+#Adapted from https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.describe.html
 #creates a dataframe of the data
 maxsepal = dataframe.idmax(0,True)
 maxsepal = pd.dataframe.idmax(0,True)
@@ -29,6 +36,8 @@ maxsepal = pd.Datframe.idmax(0,True)
 maxsepal = Dataframe.idmax(0,True)
 maxsepal = pd.Dataframe.idxmax(0,True)
 # attempted to get the max length of the data column sepal length in column one
+#Adapted from https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.describe.html
+
 
 
 
