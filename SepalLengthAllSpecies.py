a9a2d8041d257d690e6abda351ebed60276920ba
mariesweeney900 <47941381+mariesweeney900@users.noreply.github.com>
2019-04-28 23:26:26 +0100

    Update SepalLengthAllSpecies.py

--- SepalLengthAllSpecies.py
+++ SepalLengthAllSpecies.py
@@ -13,6 +13,7 @@ Setosa = dataframe.loc[dataframe['species'] == 'setosa']
 Versicolor = dataframe.loc[dataframe['species'] == 'versicolor']
 Virginica = dataframe.loc[dataframe['species'] == 'virginica']
 #creates a dataframe of each species and asssigns them
+#Adapted from https://pandas.pydata.org/pandas-docs/stable/getting_started/10min.html
 
 number_of_samples = int(50)
 #defines the number of samples and assigns them an integer
@@ -21,10 +22,11 @@ plot.scatter(range(number_of_samples),Setosa["sepal_length"], color = "yellow")
 plot.scatter(range(number_of_samples),Versicolor["sepal_length"], color = "red")
 plot.scatter(range(number_of_samples),Virginica["sepal_length"], color = "blue")
 #utilises the plot.scatter function in matplotlib to create a scatter plot, uses the range function to define y and number of samples, defines x axis as sepal length of all species and assigns them their respective colours
+#Adapted from https://pythonspot.com/matplotlib-scatterplot/
 
 plot.title("Sepal Length Setosa, Versicolor and Virginica")
 plot.xlabel('Number_Samples')
 plot.ylabel('Sepal_Length')
-
 plot.show()
 #uses the title label and show functions in matplotlib to give a title to the plot and label the x and y axis and then shows the plot
+#Adapted from https://pythonspot.com/matplotlib-scatterplot/
